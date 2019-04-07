5d1ad79bd2737ac9e8d0e8924b229e6df8775dff
BOUTKHOURST Youness <youness.bout@gmail.com>
2019-04-07 13:47:28 +0100

    Inital Commit: create project structure

---
+++ exposition/src/main/java/com/nexio/exercice/Application.java
@@ -0,0 +1,45 @@
+package com.nexio.exercice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+
+@ComponentScan(basePackages = {"com.nexio"})
+@SpringBootApplication
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+
+        String hostAddress = "localhost";
+
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "com.nexio.exercice.Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n\t" +
+                        "External: \t{}://{}:{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                hostAddress,
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
+    }
+}
